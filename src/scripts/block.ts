e3781399ca36c1f4464b6f643efa2aae8743766b
Jonathon Toon <jonathontoon@gmail.com>
2021-05-22 16:28:26 +1000

    clean

--- src/scripts/block.ts
+++
@@ -1,9 +0,0 @@
-import Sprite from "./sprite";
-
-class Block extends Sprite {
-	constructor(x: number, y: number, size: number) {
-		super(x, y, size, "#FFFF00");
-	};
-};
-
-export default Block;
--- src/scripts/tile.ts
+++
@@ -1,9 +0,0 @@
-import Sprite from "./sprite";
-
-class Tile extends Sprite {
-	constructor(x: number, y: number, size: number) {
-		super(x, y, size, "#191b26");
-	};
-};
-
-export default Tile;
