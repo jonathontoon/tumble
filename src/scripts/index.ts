4281fa140505f5df88c25dca7eca758b967f8bea
Jonathon Toon <jonathontoon@gmail.com>
2021-05-24 23:24:41 +1000

    update

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,7 +2,7 @@ interface Tile {
 	row: number;
 	column: number;
 	color: string;
-	marked: boolean;
+	visible: boolean;
 };
 
 interface Coordinates {
@@ -14,7 +14,7 @@ interface Coordinates {
 
 	const tileSize: number = 64;
 	const numberOfTiles: number = 9;
-	const tileColors: string[] = ["red", "green", "blue", "purple", "orange"];
+	const tileColors: string[] = ["red", "green", "blue", "orange", "purple"];
 	
 	const canvasElement: HTMLCanvasElement = document.getElementById("viewport") as HTMLCanvasElement;
 	const canvasContext: CanvasRenderingContext2D = canvasElement.getContext("2d") as CanvasRenderingContext2D;
@@ -29,82 +29,77 @@ interface Coordinates {
 	// 	return tileBoard[row][column].color === color;
 	// };
 
-	function findMatchesForTile(row: number, column: number, color: string): number {
-		if (tileBoard[row][column].color === "black" 
-			|| tileBoard[row][column].marked
-			|| tileBoard[row][column].color !== color) {
-			return 0;
-		}
-
-		tileBoard[row][column].marked = true;
-		
-		let matches = 1;
-		if (row > 0) {
-			matches += findMatchesForTile(row - 1, column, color);
-		}
-		if (row < numberOfTiles - 1) {
-			matches += findMatchesForTile(row + 1, column, color);
-		}
-		if (column > 0) {
-			matches += findMatchesForTile(row, column - 1, color);
-		}
-		if (column < numberOfTiles - 1) {
-			matches += findMatchesForTile(row, column + 1, color);
-		}
-
-		return matches;
+	function withinBounds(row: number, column: number): boolean {
+		return row >= 0 && row < numberOfTiles && column >= 0 && column < numberOfTiles;
 	};
 
-	function clearTile(row: number, column: number, color: string): void {
-		if (findMatchesForTile(row, column, color) < 2) {
-			tileBoard[row][column].marked = false;
-			return;
-		}
+	function tilesToMatch(row: number, column: number, color: string): void {
+		const tilesChecked: Tile[] = [];
+		const tilesToCheck: Tile[] = [];
+		tilesToCheck.push(tileBoard[row][column]);
+
+		while (tilesToCheck.length > 0) {
+			const tile: Tile | undefined = tileBoard[row][column];
+
+			if (tile && (tile.color !== color || !tile.visible || tilesChecked.includes(tile))) {
+				continue;
+			}
+
+			tile.visible = false;
+
+			tilesChecked.push(tile);
+
+			tilesToCheck.push(tileBoard[row - 1][column]);
+			tilesToCheck.push(tileBoard[row][column + 1]);
+			tilesToCheck.push(tileBoard[row + 1][column]);
+			tilesToCheck.push(tileBoard[row][column - 1]);
+		};
 	};
 	
-	function dropMarkedBlocks(): void {
-		for (let col = 0; col < numberOfTiles; col++) {
-			for (let row = numberOfTiles - 1; row >= 0; row--) {
-				if (tileBoard[row][col].marked) {
-					for (let colrow = row; colrow >= 0; colrow--) {
-						if (colrow > 0) {
-							tileBoard[colrow][col].color = tileBoard[colrow - 1][col].color;
-							tileBoard[colrow][col].marked = tileBoard[colrow - 1][col].marked;
-						} else {
-							tileBoard[colrow][col].color = "black";
-							tileBoard[colrow][col].marked = false;
+	function compressRows(): void {
+		let currentRow: number;
+		
+		for (let column: number = 0; column < numberOfTiles; column++) {
+			for (let row: number = numberOfTiles - 1; row >= 0; row--) {
+				if (!tileBoard[row][column].visible) {
+					currentRow = row - 1;
+
+					while(currentRow >= 0) {
+						if (tileBoard[currentRow][column].visible) {
+							tileBoard[row][column].row = tileBoard[currentRow][column].row;
+							tileBoard[row][column].column = tileBoard[currentRow][column].column;
+							tileBoard[row][column].color = tileBoard[currentRow][column].color;
+
+							currentRow = 0;
 						}
+						currentRow--;
 					}
-					row++;
 				}
 			}
 		}
 	}
 	
-	function shiftClearedRows(): void {
-		for (let col = 0; col < numberOfTiles; col++) {
-			while (tileBoard[numberOfTiles - 1][col].color === "black") {
-				for (let shiftcol = col; shiftcol < numberOfTiles - 1; shiftcol++) {
-					for (let row = 0; row < numberOfTiles; row++) {
-						tileBoard[row][shiftcol].color = tileBoard[row][shiftcol + 1].color;
-						tileBoard[row][shiftcol].marked = tileBoard[row][shiftcol + 1].marked;
+	function compressColumns(): void {
+		let currentColumn: number;
+
+		for(let column: number = 0; column < numberOfTiles; column++) {
+			if (!tileBoard[numberOfTiles - 1][column].visible) {
+				currentColumn = column + 1;
+ 
+				while (currentColumn < numberOfTiles) {
+					if (tileBoard[numberOfTiles - 1][currentColumn].visible) {
+						for (let row: number = 0; row < numberOfTiles; row++) {
+							if(tileBoard[row][currentColumn].visible) {
+								tileBoard[row][column].row = tileBoard[row][currentColumn].row;
+								tileBoard[row][column].column = tileBoard[row][currentColumn].column;
+								tileBoard[row][column].color = tileBoard[row][currentColumn].color;
+							}
+						}
+		
+						currentColumn = numberOfTiles;
 					}
-				}
-				
-				for (let row = 0; row < numberOfTiles; row++) {
-					tileBoard[row][numberOfTiles - 1].color = "black";
-					tileBoard[row][numberOfTiles - 1].marked = false;
-				}
-				
-				let allblack = true;
-				for (let examcol = numberOfTiles - 1; examcol > col; examcol--) {
-					if (tileBoard[numberOfTiles - 1][examcol].color !== "black") {
-						allblack = false;
-					}
-				}
-				
-				if (allblack) {
-					break;
+		
+					currentColumn++;
 				}
 			}
 		}
@@ -120,7 +115,7 @@ interface Coordinates {
 					row: i,
 					column: j,
 					color: tileColors[Math.floor(Math.random() * tileColors.length)],
-					marked: false
+					visible: true
 				};
 			}
 		}
@@ -133,25 +128,29 @@ interface Coordinates {
 
 		for (let i = 0; i < numberOfTiles; i++) {
 			for (let j = 0; j < numberOfTiles; j++) {
-				canvasContext.beginPath();
-				canvasContext.fillStyle = tileBoard[i][j].color;
-				canvasContext.rect(tileBoard[i][j].column * tileSize, tileBoard[i][j].row * tileSize, tileSize, tileSize);
-				canvasContext.fill();
-				canvasContext.closePath();
+				const tile: Tile = tileBoard[i][j];
+				if (tile.visible) {
+					canvasContext.beginPath();
+					canvasContext.fillStyle = tile.color;
+					canvasContext.rect(tile.row * tileSize, tile.column * tileSize, tileSize, tileSize);
+					canvasContext.fill();
+					canvasContext.closePath();
+				}
 			}
 		}
 	};
 
-	function getMousePosition(canvas: HTMLCanvasElement, e: MouseEvent): Coordinates {
-		const rect: DOMRect = canvas.getBoundingClientRect();
-		return {
-			x: e.clientX - rect.left,
-			y: e.clientY - rect.top
-		};
-	}
+	// function getMousePosition(canvas: HTMLCanvasElement, e: MouseEvent): Coordinates {
+	// 	const rect: DOMRect = canvas.getBoundingClientRect();
+	// 	return {
+	// 		x: e.clientX - rect.left,
+	// 		y: e.clientY - rect.top
+	// 	};
+	// }
 
 	function handleDOMContentLoaded(): void {
 		tileBoard = generateBoard();
+		tilesToMatch(5, 5, tileBoard[5][5].color);
 		console.log(tileBoard);
 	};
 
@@ -160,18 +159,18 @@ interface Coordinates {
 		window.requestAnimationFrame(handleRequestAnimationFrame);
 	};
 
-	function handleOnClick(event: MouseEvent): void {
-		const mousePosition = getMousePosition(canvasElement, event);
-		const row = Math.floor(mousePosition.y / tileSize);
-		const column = Math.floor(mousePosition.x / tileSize);
+	// function handleOnClick(event: MouseEvent): void {
+	// 	const mousePosition = getMousePosition(canvasElement, event);
+	// 	const row = Math.floor(mousePosition.x / tileSize);
+	// 	const column = Math.floor(mousePosition.y / tileSize);
 
-		clearTile(row, column, tileBoard[row][column].color);
-		dropMarkedBlocks();
-		shiftClearedRows();
-	};
+	// 	// tilesToMatch(row, column, tileBoard[row][column].color);
+	// 	// compressRows();
+	// 	// compressColumns();
+	// };
 
 	document.addEventListener("DOMContentLoaded", (handleDOMContentLoaded), false);
 	window.requestAnimationFrame(handleRequestAnimationFrame);
-	canvasElement.addEventListener("click", handleOnClick, false);
+	// canvasElement.addEventListener("click", handleOnClick, false);
 
 })();
